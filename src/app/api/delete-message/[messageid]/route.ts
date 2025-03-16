7669aea73a314e9aae9473d7f54b00008c63e297
justsurfingit <panwararun116@gmail.com>
2025-03-16 22:58:05 +0530

    fixed delete api route

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -3,17 +3,15 @@
 import dbConnect from "@/lib/dbConnect";
 import userModel from "@/model/User";
 import { NextOptions } from "../../auth/[...nextauth]/options";
-import NextAuth, { getServerSession } from "next-auth";
-import { useParams } from "next/navigation";
-import { Session } from "inspector/promises";
+import { getServerSession } from "next-auth";
 
-// but only for those who are authenticated
 export async function DELETE(
   request: Request,
   { params }: { params: { messageid: string } }
 ) {
   const messageId = params.messageid;
   const session = await getServerSession(NextOptions);
+
   if (!session || !session?.user) {
     return Response.json(
       {
@@ -38,20 +36,23 @@ export async function DELETE(
       }
     );
   }
+
   try {
     await dbConnect();
     const updateResult = await userModel.updateOne(
       { _id: sessionUser.id },
       { $pull: { messages: { _id: messageId } } }
     );
+
     if (updateResult.modifiedCount === 0) {
       return Response.json(
         { message: "Message not found or already deleted", success: false },
         { status: 404 }
       );
     }
+
     return Response.json(
-      { message: "Message deleted Succesfully", success: true },
+      { message: "Message deleted Successfully", success: true },
       { status: 200 }
     );
   } catch (err) {
@@ -61,5 +62,4 @@ export async function DELETE(
       { status: 500 }
     );
   }
-  // now okay we have a user and now we will check whether the message to be deleted is his message or not
 }
